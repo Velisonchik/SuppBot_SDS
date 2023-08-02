6cf0e813e8c6226d5e1d0d6d32de695a30803192
velisonchik <velisonchik@gmail.com>
2023-08-02 13:11:31 +0300

    Fix кода и Fix README

--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import threading
+from pprint import pprint
 
 from time import sleep
 from ad import get_ids_from_ad
@@ -30,6 +31,7 @@ def parse_msg(mas):
             d[i[:i.index('=')].lower()] = i[i.index('=') + 1:].lower()
             mas[mas.index(i)] = ''
     d['text'] = ' '.join(mas).strip()
+    d['t'] = d['t'].replace(',', '.')
     return d
 
 
@@ -47,24 +49,45 @@ async def help_message(message: Message):
     await message.answer(HELP_TEXT_FOR_BOT, parse_mode=enums.parse_mode.ParseMode.HTML)
 
 
-@dp.message(lambda x: x.content_type != ContentType.TEXT)
+@dp.message(lambda x: x.content_type not in (ContentType.DOCUMENT, ContentType.TEXT, ContentType.PHOTO))
 async def not_text(message: Message):
     global allow_ids
+    pprint(message)
     logging.warning(f'{message.from_user.id, message.content_type} send message ANOTHER type')
     users_dict[message.from_user.id] = {}
     await message.reply('Почитай пожалуйста /help, я просил не присылать мне какие-либо вложения.\nСпасибо.')
 
 
-@dp.message(lambda x: str(x.from_user.id) in allow_ids and x.forward_from)
+@dp.message(lambda x: str(x.from_user.id) in allow_ids and x.forward_from and 'text' in users_dict[x.from_user.id])
 async def adding_text_from_forward(message: Message):
+    global allow_ids
+    users_dict[message.from_user.id]['is_forward'] = True
     try:
-        global allow_ids
         if 'text' in users_dict[message.from_user.id].keys():
             users_dict[message.from_user.id]['comment'] = users_dict[message.from_user.id]['text']
             users_dict[message.from_user.id]['text'] = ''
         if 'j' in users_dict[message.from_user.id].keys() and 't' in users_dict[message.from_user.id].keys():
             await message.answer('Понял что есть форвард.')
-            users_dict[message.from_user.id]['text'] = message.text
+
+            if message.caption:
+                users_dict[message.from_user.id]['text'] = message.caption
+            elif message.text:
+                users_dict[message.from_user.id]['text'] = message.text
+            elif users_dict[message.from_user.id]['comment'] != '':
+                users_dict[message.from_user.id]['text'] = users_dict[message.from_user.id]['comment']
+                users_dict[message.from_user.id]['comment'] = ''
+            else:
+                users_dict[message.from_user.id]['text'] = ''
+                await message.answer('Тема не может быть пустой. Попробуй еще раз.')
+
+            if message.content_type == ContentType.PHOTO:
+                await bot.download(message.photo[-1], f'{message.photo[-1].file_unique_id}.jpg')
+                users_dict[message.from_user.id]['file_path'] = f'{message.photo[-1].file_unique_id}.jpg'
+
+            elif message.content_type == ContentType.DOCUMENT:
+                await bot.download(message.document, message.document.file_name)
+                users_dict[message.from_user.id]['file_path'] = message.document.file_name
+
             users_dict[message.from_user.id]['forward_user_id'] = message.forward_from.id
 
             if str(users_dict[message.from_user.id]['forward_user_id']) in allow_ids:
@@ -82,7 +105,10 @@ async def adding_text_from_forward(message: Message):
                 username=allow_ids[str(message.from_user.id)],
                 tracker_id_in_str=users_dict[message.from_user.id]['j'],
                 elapsed_time=float(users_dict[message.from_user.id]['t']),
-                author_issue=username_from_ad, comment=users_dict[message.from_user.id]['comment'])
+                author_issue=username_from_ad, comment=users_dict[message.from_user.id]['comment'],
+                file_path='' if 'file_path' not in users_dict[message.from_user.id].keys() else
+                users_dict[message.from_user.id]['file_path']
+            )
             await message.reply(users_dict[message.from_user.id]['issue_URI'])
             logging.info(f"Issue CREATED WITH FORWARD {users_dict[message.from_user.id]}")
             users_dict[message.from_user.id] = {}
@@ -100,6 +126,7 @@ async def adding_text_from_forward(message: Message):
 @dp.message(lambda x: str(x.from_user.id) in allow_ids and 't=' in x.text.lower() and 'j=' in x.text.lower())
 async def first_create_issue(message: Message):
     try:
+
         users_dict[message.from_user.id] = {}
         users_dict[message.from_user.id] = parse_msg(message.text.split())
         print(users_dict)
@@ -125,12 +152,14 @@ async def first_create_issue(message: Message):
                     users_dict[message.from_user.id] = {}
             else:
                 await message.answer('Я тебя не понимаю. Попробуй еще раз.')
-                users_dict[message.from_user.id] = {}
                 logging.warning(f'Error in checking len items {users_dict[message.from_user.id]}')
+                users_dict[message.from_user.id] = {}
         else:
-            await message.answer('Я хочу еще текст. Попробуй еще раз.')
-            users_dict[message.from_user.id] = {}
             logging.warning(f'Error in checking TEXT items {users_dict[message.from_user.id]}')
+            await asyncio.sleep(2)
+            if 'is_forward' not in users_dict[message.from_user.id].keys():
+                await message.answer('Я хочу еще текст. Попробуй еще раз.')
+                users_dict[message.from_user.id] = {}
     except Exception as e:
         print(e)
         await message.answer('Не вышло, надо смотреть логи.')
@@ -150,7 +179,8 @@ async def message_for_all(message: Message):
     global allow_ids
     users_dict[message.from_user.id] = {}
     logging.warning(
-        f'User ID {message.from_user.id}:{message.from_user.username} {message.from_user.first_name}:{message.from_user.last_name} not in ALLOWED')
+        f'User ID {message.from_user.id}:{message.from_user.username} {message.from_user.first_name}:'
+        f'{message.from_user.last_name} not in ALLOWED')
     await message.reply("Я тебя не знаю, пройди аутентификацю у @sds_corp_bot, и возвращайся!")
 
 
--- support.py
+++ support.py
@@ -1,3 +1,5 @@
+import os
+
 from reqs import SUPPORT_SITE, SUPPORT_API_KEY, SUPPORT_VERSION, PROJECT_ID_FOR_NEW_ISSUE
 from redminelib import Redmine
 
@@ -5,7 +7,7 @@ tracker_ids = {'i': 2, 'c': 3, 'e': 1}
 
 
 def create_new_issue(subject: str, description: str, username: str, tracker_id_in_str: str, elapsed_time: float,
-                     author_issue='', comment=''):
+                     author_issue='', comment='', file_path=''):
     print('Result of create_new_issue: ')
     if not author_issue:
         author_issue = username
@@ -17,18 +19,23 @@ def create_new_issue(subject: str, description: str, username: str, tracker_id_i
         return f'Такого пользователя {author_issue} не нашел в саппорте'
     redmine_for_author_issue = Redmine(SUPPORT_SITE, key=redmine_adm.user.get(author_issue_id).api_key,
                                        version=SUPPORT_VERSION)
-    issue = redmine_for_author_issue.issue.create(
-        project_id=PROJECT_ID_FOR_NEW_ISSUE,
-        subject=subject,
-        tracker_id=tracker_ids[tracker_id_in_str],
-        description=description,
-        status_id=5,
-        priority_id=4,
-        category_id=186,
-        fixed_version_id=4,
-        assigned_to_id=assigned_to_id,
-        done_ratio=40,
-    )
+
+    issue = redmine_for_author_issue.issue.new()
+    issue.project_id = PROJECT_ID_FOR_NEW_ISSUE
+    issue.subject = subject
+    issue.tracker_id = tracker_ids[tracker_id_in_str]
+    issue.description = description
+    issue.status_id = 5
+    issue.priority_id = 4
+    issue.category_id = 186
+    issue.fixed_version_id = 4
+    issue.assigned_to_id = assigned_to_id
+    issue.done_ratio = 40
+    if file_path != '':
+        issue.uploads = [{'path': f'{file_path}', 'filename': f'{file_path}'}]
+    issue.save()
+    os.remove(file_path)
+
     redmine_for_current_user = Redmine(SUPPORT_SITE, key=redmine_adm.user.get(assigned_to_id).api_key,
                                        version=SUPPORT_VERSION)
 
@@ -36,12 +43,16 @@ def create_new_issue(subject: str, description: str, username: str, tracker_id_i
         issue_id=issue.id,
         hours=elapsed_time,
         user_id=assigned_to_id,
-        comments=comment
+        # comments=comment
     )
+    if comment != '':
+        redmine_for_current_user.issue.update(resource_id=issue.id, notes=comment)
+
     print(issue.url)
     return issue.url
 
 
 if __name__ == '__main__':
-    print(create_new_issue(subject='Test SuppBot', description='Description', username='', tracker_id_in_str='c',
-                           elapsed_time=0.8))
+    print(create_new_issue(subject='Test SuppBot', description='Description', username='kashanyan.v',
+                           tracker_id_in_str='c',
+                           elapsed_time=0.8, comment='Тестовый коммент'))
